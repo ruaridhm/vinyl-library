76793caeeea551bf87191be08d8a4825853ca1d3
ruaridhm <ruaridh.mcguckin@gmail.com>
2020-09-12 13:26:25 +0100

    basic nav

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className='navbar'>
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+      <ul className='nav-links'>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.func.isRequired,
+  icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: 'Vinyl Library',
+  icon: 'fas fa-record-vinyl',
+};
+
+export default Navbar;
