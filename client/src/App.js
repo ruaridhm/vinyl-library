c84df27ebeb64a5be69eb6364845a58adea56c1a
ruaridhm <ruaridh.mcguckin@gmail.com>
2020-09-16 13:49:19 +0100

    register & login forms,state,auth,routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,32 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import RecordState from './context/record/RecordState';
+import AuthState from './context/auth/AuthState';
+
 import './App.css';
 
 const App = () => {
   return (
-    <RecordState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </RecordState>
+    <AuthState>
+      <RecordState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </RecordState>
+    </AuthState>
   );
 };
 
