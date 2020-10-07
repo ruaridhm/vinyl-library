2cbec49f66b7f39be90c6a4c2da61b120b2d4010
ruaridhm <ruaridh.mcguckin@gmail.com>
2020-10-07 02:28:38 +0100

    Removed initial route

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-// app.get('/', (req, res) =>
-//   res.json({ msg: 'Welcome to the Vinyl Library API' })
-// );
-
 //Route Definitions
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
